d87de6a59e150c1fe4a49f4128abf483104ce0b9
shansho272-source <shansho272@gmail.com>
2026-05-05 19:58:25 +0400

    Add files via upload

---
+++ Landingsite _ Landingsite_files/config.js
@@ -0,0 +1,7 @@
+(function() {
+  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
+  window._POSTHOG_REMOTE_CONFIG['phc_IS9NHfACBg6Ul9wpMrfnfiBB7DvwFUYn6VPfgoiLi2c'] = {
+    config: {"analytics":{"endpoint":"/i/v0/e/"},"autocaptureExceptions":false,"autocapture_opt_out":true,"captureDeadClicks":false,"capturePerformance":{"network_timing":true,"web_vitals":false,"web_vitals_allowed_metrics":null},"conversations":false,"defaultIdentifiedOnly":true,"elementsChainAsString":true,"errorTracking":{"autocaptureExceptions":false,"suppressionRules":[]},"hasFeatureFlags":true,"heatmaps":false,"logs":{"captureConsoleLogs":false},"productTours":false,"sdkVersion":{"requested":"1"},"sessionRecording":{"canvasFps":null,"canvasQuality":null,"consoleLogRecordingEnabled":true,"endpoint":"/s/","eventTriggers":[],"linkedFlag":null,"masking":null,"minimumDurationMilliseconds":15000,"networkPayloadCapture":null,"recordCanvas":false,"recorderVersion":"v2","sampleRate":null,"scriptConfig":{"script":"posthog-recorder"},"triggerMatchType":null,"urlBlocklist":[],"urlTriggers":[],"version":1},"supportedCompression":["gzip","gzip-js"],"surveys":false,"token":"phc_IS9NHfACBg6Ul9wpMrfnfiBB7DvwFUYn6VPfgoiLi2c"},
+    siteApps: []
+  }
+})();
